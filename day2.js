e229a0ee804151e5f7780bdec4e7180f8bfa341b
tun35770 <tun35770@temple.edu>
2023-12-02 17:18:49 -0500

    Day 2 Part 2

--- day2.js
+++ day2.js
@@ -25,6 +25,7 @@ const main = async () => {
         'blue': 14
     }
 
+    //part 1
     let count = 0;
 
     for(let i = 0; i < input.length; i++){
@@ -48,6 +49,37 @@ const main = async () => {
     }
 
     console.log(count);
+
+
+    //part 2
+    let total_power = 0;
+
+    for(let i = 0; i < input.length; i++){
+        const line = input[i].split(' ');
+          
+        let maxRed = 0, maxGreen = 0, maxBlue = 0;
+
+        for(let j = 2; j < line.length; j+=2){
+            const num = Number(line[j]);
+            let color = line[j+1];
+            if(color.charAt(color.length-1) === ',' || color.charAt(color.length-1) === ';')
+                color = color.substring(0, color.length-1);
+            
+            switch(color){
+                case 'red': maxRed = Math.max(maxRed, num);
+                    break;
+                case 'green': maxGreen = Math.max(maxGreen, num);
+                    break;
+                case 'blue': maxBlue = Math.max(maxBlue, num);
+                    break;
+            }
+
+        }
+
+        total_power += maxRed * maxGreen * maxBlue
+    }
+
+    console.log(total_power);
 }
 
 main();
