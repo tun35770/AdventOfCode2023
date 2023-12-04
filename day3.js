f6e58c1487f3a37d7ecf3c4c7d958a2c8db5755b
tun35770 <tun35770@temple.edu>
2023-12-04 13:47:53 -0500

    Day 3 Part 2

--- day3.js
+++ day3.js
@@ -81,6 +81,72 @@ const main = async () => {
     }
 
     console.log(sum);
+
+
+    //part 2
+    sum = 0;
+    let map = new Map();
+
+    const isStar = (row, col) => {
+        return g[row][col] === '*';
+    }
+
+    const checkAdjToStar = (row, col, w) => {
+        let idx = [-1, -1];
+       
+        for(let i = row - 1; i <= row + 1; i++){
+            for(let j = col - 1; j <= col + w; j++){
+                if(i >= 0 && i < g.length && col >= 0 && col < g[i].length && isStar(i, j)){
+                    idx = [i, j];
+                    break;
+                }
+            }
+
+            if(idx[0] !== -1) return idx;
+        }
+        return idx;
+    }
+
+    for(let i = 0; i < g.length; i++){
+        for(let j = 0; j < g[i].length;){
+            
+            if(g[i][j] >= "0" && g[i][j] <= "9"){
+                let w = 1;
+
+                if(j < g[i].length-1 && (g[i][j+1] >= "0" && g[i][j+1] <= "9")){
+                    w = 2;
+                    if(j < g[i].length-2 && (g[i][j+2] >= "0" && g[i][j+2] <= "9")){
+                        w = 3;
+                    }
+                }
+
+                const starIdx = checkAdjToStar(i, j, w);
+                if(starIdx[0] !== -1){
+                    let num = "";
+                    for(let k = 0; k < w; k++){
+                        num += g[i][j+k];
+                    }
+    
+                    //console.log(num)
+                    if(map.has(`${starIdx[0]} ${starIdx[1]}`)){
+                        sum += map.get(`${starIdx[0]} ${starIdx[1]}`) * Number(num);
+                        
+                    }
+                        
+                    else
+                        map.set(`${starIdx[0]} ${starIdx[1]}`, [Number(num)]);
+                }
+
+                j += w;
+                continue;
+            }
+
+            j++;
+        }
+    }
+
+    console.log(sum);
+
 }
 
 main();
