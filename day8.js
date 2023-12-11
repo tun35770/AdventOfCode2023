2d203720c458bb6dc6160362739f9d8ab8159f3e
tun35770 <tun35770@temple.edu>
2023-12-11 12:29:18 -0500

    Day 8 Part 2

--- day8.js
+++ day8.js
@@ -27,6 +27,8 @@ const main = async () => {
         g[line[0]] = [line[1], line[2]];
     }
 
+
+    //part 1
     let turns = input[0];
     let here = 'AAA';
     let i = 0;
@@ -43,6 +45,63 @@ const main = async () => {
     }
 
     console.log(steps);
+
+
+    //part 2
+    const keys = Object.keys(g);
+    let q = [];
+
+    for(let i = 0; i < keys.length; i++){
+        if(keys[i].charAt(2) === 'A')
+            q.push(keys[i]);
+    }
+
+    turns = input[0];
+    let arr = [];
+
+    for(let j = 0; j < q.length; j++){
+
+        let here = q[j];
+        let i = 0;
+        let steps = 0;
+
+        while(here.charAt(2) !== 'Z'){
+            let dir = turns.charAt(i);
+
+            if(dir === 'L')
+                here = g[here][0];
+            else
+                here = g[here][1];
+
+            i = (i+1) % turns.length;
+
+            steps++;
+
+        }
+
+        arr.push(steps);
+    }
+
+    arr = arr.sort((a, b) => a - b);
+
+
+    //find LCM of each starting node's steps
+
+    //had to copy paste this part :)
+    function gcd(a, b) {
+        return !b ? a : gcd(b, a % b);
+    }
+
+    function lcm(a, b) {
+        return (a * b) / gcd(a, b);   
+    }
+
+    let multiple = arr[0];
+    arr.forEach(function(n) {
+        multiple = lcm(multiple, n);
+    });
+
+    console.log(multiple);
 }
 
 main();
